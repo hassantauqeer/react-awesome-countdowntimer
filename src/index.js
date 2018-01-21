1d4294a4dece32c67a8d70162073b381cb04a7c2
Søren Berg Glasius <soeren@glasius.dk>
2018-01-21 16:58:33 +0100

    Indentify each section by className

--- src/index.js
+++ src/index.js
@@ -82,35 +82,35 @@ class CountdownTimer extends React.Component {
             <div className="timer" style={timer}>
                 {
                     this.state.timeRemaining.months &&
-                    <div className="section" style={section}>
+                    <div className="section months" style={section}>
                         <div className="time" style={time}>{this.state.timeRemaining.months}</div>
                         <div className="label" style={label}>Months</div>
                     </div>
                 }
                 {
                     this.state.timeRemaining.days &&
-                    <div className="section" style={section}>
+                    <div className="section days" style={section}>
                         <div className="time" style={time}>{this.state.timeRemaining.days}</div>
                         <div className="label" style={label}>Days</div>
                     </div>
                 }
                 {
                     this.state.timeRemaining.h &&
-                    <div className="section" style={section}>
+                    <div className="section hours" style={section}>
                         <div className="time" style={time}>{this.state.timeRemaining.h}</div>
                         <div className="label" style={label}>Hours</div>
                     </div>
                 }
                 {
                     this.state.timeRemaining.m &&
-                    <div className="section" style={section}>
+                    <div className="section minutes" style={section}>
                         <div className="time" style={time}>{this.state.timeRemaining.m}</div>
                         <div className="label" style={label}>Minutes</div>
                     </div>
                 }
                 {
                     this.state.timeRemaining.s &&
-                    <div className="section" style={section}>
+                    <div className="section seconds" style={section}>
                         <div className="time" style={time}>{this.state.timeRemaining.s}</div>
                         <div className="label" style={label}>Seconds</div>
                     </div>
